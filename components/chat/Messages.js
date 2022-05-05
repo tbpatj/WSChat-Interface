c32a8408df29619675c39a11f69514143ccff7e6
Tim Luebke <timluebke@Nerds-MacBook-Pro-7.local>
2022-05-05 15:10:01 -0600

    fixed some stuff

--- components/chat/Messages.js
+++ components/chat/Messages.js
@@ -17,22 +17,26 @@ export default function Messages() {
     <div className="history">
       {chatLog.map((chatItem, index) => {
         return (
-          <div key={index + "msg"} className="messages-container">
-            {/* insert a div in front to move content sent by the user to the right */}
-            {chatItem.data.username === username && <div></div>}
+          <div>
+            <div key={index + "msg"} className="messages-container">
+              {/* insert a div in front to move content sent by the user to the right */}
+
+              {chatItem.data.username === username && <div></div>}
+
+              {/* the messages in the chat */}
+              <div
+                key={index + "msg"}
+                className={
+                  chatItem.data.username === username ? "my-message" : "message"
+                }
+              >
+                <i>{chatItem.data.username}:</i>
+                <br /> {chatItem.data.message}
+              </div>
+            </div>
             {index === chatLog.length - 1 && (
               <div ref={lastMessage} id="last-div"></div>
             )}
-            {/* the messages in the chat */}
-            <div
-              key={index + "msg"}
-              className={
-                chatItem.data.username === username ? "my-message" : "message"
-              }
-            >
-              <i>{chatItem.data.username}:</i>
-              <br /> {chatItem.data.message}
-            </div>
           </div>
         );
       })}
